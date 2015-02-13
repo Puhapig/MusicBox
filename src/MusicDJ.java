b26cb6fb265aab55d894dabc52daaac9365b23e5
Harvey <hjrendell@gmail.com>
2015-02-13 23:42:53 +1300

    Initial commit

---
+++ src/MusicDJ.java
@@ -0,0 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class MusicDJ extends JFrame {
+	
+	private static MusicDJ instance = null;
+	
+	protected MusicDJ() {}
+	
+	public static MusicDJ getInstance() {
+		if (instance == null) {
+			instance = new MusicDJ();
+		}
+		return instance;
+	}
+	
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				MusicDJ i = getInstance();
+				i.setVisible(true);
+				i.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				i.setSize(600, 400);	
+			}
+		});
+	}
+}
